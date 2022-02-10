1906ce979ac92b7a92f52bc6753890eb2c8c4d3b
johnnybigoo <jotalico@icloud.com>
2022-02-10 17:37:54 -0500

    Video #57 => OOP methods => surcharging p2

--- src/academy/devdojo/maratonajava/javacore/sobrecargametodos/dominio/Anime.java
+++ src/academy/devdojo/maratonajava/javacore/sobrecargametodos/dominio/Anime.java
@@ -4,6 +4,18 @@ public class Anime {
     private String nome;
     private String tipo;
     private int episodios;
+    private String genero;
+
+    public void init(String nome, String tipo, int episodios) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.episodios = episodios;
+    }
+
+    public void init(String nome, String tipo, int episodios, String genero){
+        this.init(nome, tipo, episodios);
+        this.genero = genero;
+    }
 
     public void imprime(){
         System.out.println(this.nome);
@@ -33,5 +45,13 @@ public class Anime {
 
     public void setEpisodios(int episodios) {
         this.episodios = episodios;
+    }
+
+    public String getGenero() {
+        return this.genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
     }
 }
--- src/academy/devdojo/maratonajava/javacore/sobrecargametodos/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/sobrecargametodos/test/AnimeTest01.java
@@ -5,9 +5,10 @@ import academy.devdojo.maratonajava.javacore.sobrecargametodos.dominio.Anime;
 public class AnimeTest01 {
     public static void main(String[] args) {
         Anime anime = new Anime();
-        anime.setNome("Akudama Drive");
-        anime.setTipo("TV");
-        anime.setEpisodios(12);
+        anime.init("Akudama Drive", "TV", 12, "Ação");
+//        anime.setNome("Akudama Drive");
+//        anime.setTipo("TV");
+//        anime.setEpisodios(12);
         anime.imprime();
     }
 }
