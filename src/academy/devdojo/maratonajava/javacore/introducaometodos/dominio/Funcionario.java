da15b2a2c5cffe098e8264a67445eec6b90c66e1
johnnybigoo <jotalico@icloud.com>
2022-02-10 09:54:45 -0500

    Video #53 => OOP methods  => exercise

---
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Funcionario.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.introducaometodos.dominio;
+
+public class Funcionario {
+    public String nome;
+    public int idade;
+    public double[] salarios;
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        for(double salario: salarios){
+            System.out.print(salario + " ");
+        }
+    }
+
+    public void imprimeMediaSalario(){
+        double media = 0;
+        for(double salario: salarios){
+            media += salario;
+        }
+        media /= salarios.length;
+        System.out.println("\nMedia salarial: " + media);
+
+    }
+}
